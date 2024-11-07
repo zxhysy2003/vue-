fafe73174eeea7cacab438bdb5b5b292771ecf34
MrShi <1499172392@qq.com>
2024-11-07 18:18:49 +0800

    header组件创建和样式编写

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -4,10 +4,15 @@
       <!-- <el-aside width="200px">Aside</el-aside> -->
       <Aside />
       <el-container>
-        <el-header>Header</el-header>
+
+        <el-header>
+          <Header />
+        </el-header>
+        
         <el-main>
           <RouterView />
         </el-main>
+        
       </el-container>
     </el-container>
   </div>
@@ -16,6 +21,7 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import Aside from '../components/aside.vue'
+import Header from '../components/navHeader.vue'
 </script>
 
 <style lang="less" scoped>
